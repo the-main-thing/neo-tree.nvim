ab9ca8b7278496031f6fee9a4d6db3de50e9adf8
the main thing <pelagusrem@gmail.com>
2025-01-19 21:14:34 +0400

    fix for stupid boolean condition

--- lua/neo-tree/sources/filesystem/commands.lua
+++ lua/neo-tree/sources/filesystem/commands.lua
@@ -75,7 +75,7 @@ local function on_before_move(source, dest, callback)
     or ends_with(dest, ".js")
     or ends_with(dest, ".jsx")
   local client = get_ts_client()
-  if ts ~= false then
+  if ts == false then
     callback(source, dest)
     return
   end
@@ -89,17 +89,14 @@ local function on_before_move(source, dest, callback)
     },
   }
 
-	vim.notify("making willRename request")
   client.request("workspace/willRenameFiles", params, function(err, result)
     if err then
       vim.notify("Error sending applyRenameFile" .. vim.inspect(err), vim.log.levels.ERROR)
       return callback(source, dest)
     end
     if result then
-			vim.notify("applying changes")
+      vim.notify("Related imports has been updated")
       vim.lsp.util.apply_workspace_edit(result, "utf-8")
-    else
-      vim.notify("No changes required for the rename.", vim.log.levels.INFO)
     end
     return callback(source, dest)
   end)
